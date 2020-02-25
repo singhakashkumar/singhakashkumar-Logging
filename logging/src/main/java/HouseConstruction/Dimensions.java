bbefb929f13686e806a9bfa523016a5df96957d9
singhakashkumar <singhakashkumar.a@gmail.com>
2020-02-25 14:18:04 +0530

    Logging

---
+++ logging/src/main/java/HouseConstruction/Dimensions.java
@@ -0,0 +1,20 @@
+//package HouseConstruction;
+//
+//public class Dimensions {
+//
+//}
+package HouseConstruction;
+
+public class Dimensions {
+    
+	  Integer length,width;
+	 
+	  public Dimensions(){
+		  
+	  }
+	  public Dimensions(Integer length, Integer width){
+		this.length=length;
+		this.width=width;
+	  }
+	  
+}
